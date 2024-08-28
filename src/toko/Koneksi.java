25ec17e05b8055eaea5acbacacf433dead45116f
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-08-28 18:56:29 +0700

    Fix Code

--- src/toko/Koneksi.java
+++ src/toko/Koneksi.java
@@ -1,31 +1,29 @@
+package toko;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author Ilham_Oktavian
- */
-public class koneksi {
+public class Koneksi {
+
     private static Connection koneksi;
-    
+
     public static Connection getKoneksi() {
-        if(koneksi == null) {
+        if (koneksi == null) {
             try {
                 String url = "jdbc:mysql://localhost:3306/penjualan";
                 String user = "root";
                 String password = "";
-                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
                 koneksi = DriverManager.getConnection(url, user, password);
-                System.out.println("Berhasil");
-            } catch(Exception e) {
-                System.out.println("Error");
+                System.out.println("Connection successful!");
+            } catch (SQLException e) {
+                System.out.println("Connection failed: " + e.getMessage());
             }
         }
         return koneksi;
     }
-    
-    public static void main(String args[]) {
+
+    public static void main(String[] args) {
         getKoneksi();
     }
 }
